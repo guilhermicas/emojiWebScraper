80eb38dba7f090e186df42f16e51a671821aec2d
Guilherme Silva <sotrabalhosgui@hotmail.com>
2020-12-02 15:49:16 +0000

    Inserção de comentários mais explicitos e em português :^)

--- webScraper.py
+++ webScraper.py
@@ -1,18 +1,14 @@
-# Go to https://unicode.org/emoji/charts/full-emoji-list.html
-# In the emoji table extract line by line the UTF-8 Code, the emoji itself and the name of the emoji
-# In this order (Emoji, Emoji Name, ASCII code)
 import requests
 from bs4 import BeautifulSoup
 
+# HTML get request da página dos emojis
 EMOJI_URL = 'https://unicode.org/emoji/charts/full-emoji-list.html'
 page = requests.get(EMOJI_URL)
 
+# Conversão para BeautifulSoup para poder efetuar pesquisas nos elementos da página
 soup = BeautifulSoup(page.content, 'html.parser')
 
-# Do a while true loop, and find the parent of the node which the class is rchars, and the content of that node is a number from 1 till none is found
-# Once i find the parent element, get the Emoji, Emoji Name, and ASCII Code, put them in a string with a linebreak on the end
-# Once i've gone through every node, pipe the output into a file
-
+# Implementação da estrutura de pesquisa para obter Emoji, nome do emoji (em inglês devido ao site), e o seu código unicode
 response = soup.find_all("td", class_="rchars")
 
 with open("./emojis", "w") as listaEmojis:
@@ -25,10 +21,11 @@ with open("./emojis", "w") as listaEmojis:
 
         # Emoji Name
         listaEmojis.write(parent.find(
-            'td', class_="name").contents[0] + " ")
+            'td', class_="name").contents[0].title() + " ")
 
         # Emoji ASCII
         listaEmojis.write(parent.find('td', class_="code").find(
             'a').contents[0] + "\n")
 
+# Programa completo
 input("[Scraping completado]")
