795ff22a70adc1926d6fe24b416bbeb8dff8a4f0
Guilherme Silva <sotrabalhosgui@hotmail.com>
2020-12-02 15:38:18 +0000

    Implementação de scrape completo da página e guardar o emoticon, nome do emoji e o seu código de unicode

--- webScraper.py
+++ webScraper.py
@@ -4,8 +4,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://unicode.org/emoji/charts/full-emoji-list.html'
-page = requests.get(URL)
+EMOJI_URL = 'https://unicode.org/emoji/charts/full-emoji-list.html'
+page = requests.get(EMOJI_URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
@@ -13,11 +13,22 @@ soup = BeautifulSoup(page.content, 'html.parser')
 # Once i find the parent element, get the Emoji, Emoji Name, and ASCII Code, put them in a string with a linebreak on the end
 # Once i've gone through every node, pipe the output into a file
 
-res = soup.find_all("td", class_="rchars")
+response = soup.find_all("td", class_="rchars")
 
-for em in res:
-    parent = em.find_parent()
-    print(parent.find('td', class_="code").find('a').contents)
-    print(parent.find('td', class_="chars").contents)
-    print(parent.find('td', class_="name").contents)
-    break
+with open("./emojis", "w") as listaEmojis:
+    for em in response:
+        parent = em.find_parent()
+
+        # Emoji
+        listaEmojis.write(parent.find(
+            'td', class_="chars").contents[0] + " ")
+
+        # Emoji Name
+        listaEmojis.write(parent.find(
+            'td', class_="name").contents[0] + " ")
+
+        # Emoji ASCII
+        listaEmojis.write(parent.find('td', class_="code").find(
+            'a').contents[0] + "\n")
+
+input("[Scraping completado]")
